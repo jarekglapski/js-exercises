5a3ead0ce41510a165649bb0763565e56b584f4f
jarekglapski <jarek.glapski@gmail.com>
2016-10-31 14:59:40 +0100

    exercise 9

---
+++ 9.js
@@ -0,0 +1,10 @@
+//Rewrite this code from for-loop to while-loop without changing the loop logic.
+
+for (var i = 0; i < 5; i++) {
+    console.log("number", i);
+}
+
+i=-1;
+while(++i < 5) {
+    console.log("number", i);
+}
