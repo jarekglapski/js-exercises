42d48f98b797543179f902a261aa0b8ffc62cf6b
jarekglapski <jarek.glapski@gmail.com>
2016-10-31 12:04:00 +0100

    exercise 2

---
+++ 2.js
@@ -0,0 +1,12 @@
+/**
+ * Initialize two variables: admin and name
+ * Assign string root to the variable admin
+ * Copy the value from admin to the variable name
+ * Print to the console both values
+ */
+
+var admin, name;
+admin = 'root';
+name = admin;
+//admin = 'test';
+console.log(admin, name);
