232c08433ff216db027a5a745ae35fb8a3d154cb
jarekglapski <jarek.glapski@gmail.com>
2016-10-31 15:53:10 +0100

    exercise 11

---
+++ 11.js
@@ -0,0 +1,29 @@
+//What is the result of the following code:
+
+function a() {
+    return 1;
+}
+console.log(a);
+
+//And what of the following? What's the difference and why?
+(function g() {
+    return 1;
+});
+try {
+    console.log(g);
+} catch(err) {
+    console.log(err);
+}
+//Above is not a function declaration, so it is unknown outside its own scope. It's just a function expression
+//However, it can be executed:
+(function g() {
+    console.log('inside g');
+    return 1;
+})();
+
+//If given a name, it will become a declaration:
+var b = (function g() {
+    console.log('inside g');
+    return 1;
+})
+console.log(b);
