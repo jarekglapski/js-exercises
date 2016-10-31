923f154f6ab69a89dea4398cfa802d7266ca67f0
jarekglapski <jarek.glapski@gmail.com>
2016-10-31 14:53:38 +0100

    exercise 8

---
+++ 8.js
@@ -0,0 +1,6 @@
+// Write a for-loop that will iterate from 0 to 15.
+// For each iteration, it will check if the current number is odd or even, and print a message to the console.
+
+for (var i = 0; i < 16; i++) {
+    console.log(i + ((i % 2) ? " is odd" : " is even"));
+}
